7592343316705c1322fadecad6179b010184d592
Marketen <marcfont12@gmail.com>
2024-01-11 21:21:09 +0100

    fix variables not read properly

--- twitter.py
+++ twitter.py
@@ -13,10 +13,10 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Twitter API credentials
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-access_token = os.getenv('ACCESS_TOKEN')
-access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
